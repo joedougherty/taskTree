15d015c016037c9ab2ffa7a6423642be598da0be
Dougherty, Joseph <josepd@wharton.upenn.edu>
2013-07-26 16:03:16 -0400

    added archiving ability

--- js/tree.js
+++ js/tree.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 
   function keyShortcutNewTask(e) {
-
     if ( e.altKey && e.shiftKey && (e.keyCode == 78 || e.keyCode == 110) ) {      
       $("#wrapper").append( treeNode );
     }
   }
 
-  // solely for the saw sharpening
   function ascendLevelsrecur( el, numOfLevels ) {
     if (numOfLevels == 0) {
       return el;
@@ -15,7 +13,6 @@ $(document).ready(function() {
       var el = el.parent();
       return ascendLevelsrecur( el, numOfLevels-1);
     }
-
   }
 
   function ascendLevels( el, numOfLevels ) {  
@@ -24,11 +21,12 @@ $(document).ready(function() {
     for (var i=0; i<numOfLevels; i++) {
       el = el.parent();
     }
-
     return el;
   }
 
   document.addEventListener('keyup', keyShortcutNewTask, false);
+
+  var activeDivs = $('#wrapper, #archivedTasks');
 
   var collapsed = "imgs/collapsed.png";	
   var expanded = "imgs/expanded.png";
@@ -45,110 +43,104 @@ $(document).ready(function() {
 	           '</ul>' +
 		   '</div>';
 
+  var parentLevelTreeNode = treeNode.replace('<div class="task">', '<div class="task parentLevel">');
+
   var allTasks = localStorage.getItem('allTasks');
+  var archivedTasks = localStorage.getItem('archivedTasks');
 
   if ( allTasks ) {
-    $("#wrapper").append( allTasks );
+    $('#wrapper').append( allTasks );
+    $('#archivedTasks').append( archivedTasks );
   } else {
-    $("#wrapper").append( treeNode ); // Start 'em off with something
+    $('#wrapper').append( parentLevelTreeNode ); // Start 'em off with something
   }
 
   // Add a node to the given tree
   $("button.addNode").click(function() {
-    $("#wrapper").append( treeNode );
+    $("#wrapper").append( parentLevelTreeNode );
   });      	 
 
   // Add nodes
-  $("div#wrapper").on("click", ".addNode", function(e){
-
+  $('#wrapper').on("click", ".addNode", function(e){
     $(this).parent().append( treeNode );
     $(this).parent().children("div").fadeIn();
     
     $(this).parent().find("img.arrow:first").css('visibility', 'visible');
     $(this).parent().find("img.arrow:first").attr('src', expanded);
-  
   });
 
   // Grey out item once 'complete' is checked
-  $("#wrapper").on("click", "input.isComplete", function(event){
-  
+  activeDivs.on("click", "input.isComplete", function(event){
     // This is buggy; I must not be doing something right here...    
     if ( this.checked ) {
-     
       $(this).attr('checked', 'checked');
       $(this).parent().css('color', '#ccc');
-      
     } else {
-    
       $(this).removeAttr('checked');  
       $(this).parent().css('color', '#000');
-    
     }
-
   });
   
   // Delete chosen node
-  $("#wrapper").on('click', 'span.delNode', function(e) {
-
+  activeDivs.on('click', 'span.delNode', function(e) {
     ascendLevelsrecur($(this), 3).fadeOut('fast', function() { // .parent().parent().parent() 
-
-	var numOfChildren = $(this).parent().children('div.task').length;
-	// console.log( numOfChildren );	
+	  var numOfChildren = $(this).parent().children('div.task').length;
 	
-	if ( numOfChildren === 1 ) { // it's the last child
-	  $(this).parent().find('img.arrow').css({ 'visibility' : 'hidden' });
-	}
+	  if ( numOfChildren === 1 ) { // it's the last child
+	    $(this).parent().find('img.arrow').css({ 'visibility' : 'hidden' });
+	  }
 
-	$(this).remove();
+	  $(this).remove();
     });    
- 
   });
   
   // Expand all child nodes of parent [ slideDown() ]
-  $("#wrapper").on("click", "img.arrow", function(e) {
-       
+  activeDivs.on("click", "img.arrow", function(e) {
     // Switch to alternate arrow shape
     if ( $(this).attr('src') == expanded ) { 
-     
       // Collapse child nodes
       $(this).attr('src', collapsed); 
       $(this).parent().children("div").slideUp();
-  
     } else {
-  
       // Expand child nodes
       $(this).attr('src', expanded);
       $(this).parent().children("div").slideDown();
-  
     }   
-    
   });
 
   // Save the task list whenever the window.document object is clicked
-  // Would it better to bind this to a different event listener? If so, what?
   $(document).click(function() {
-
     /* Attach additional keyboard event for auto-save */
-
     var taskList = $("#wrapper").html();
-    localStorage.setItem('allTasks', taskList);
+    var archivedTasks = $("#archivedTasks").html();
 
+    localStorage.setItem('allTasks', taskList);
+    localStorage.setItem('archivedTasks', archivedTasks);
   });
   
   // Delete localStorage task list
   $("button.clearLS").click(function() {   
-  
     if ( confirm(" You're sure? They'll disappear...never to be seen again. ") ) {
- 
       window.localStorage.removeItem('allTasks'); 
       $("#wrapper").html("");
-      
     } else {
-    
       return false;
-    
     }
+  });
 
+  // Archive completed items
+  $('button.archiveItems').bind('click', function() {
+    // Find all parent level items that are checked.
+    var parentLevelItems = $('.parentLevel');
+    
+    parentLevelItems.each(function() {
+      parentItem = $(this);
+      var checkbox = parentItem.find('.isComplete');
+    
+      if (checkbox.attr('checked') === 'checked') {
+        $('#archivedTasks').prepend( parentItem );
+      }
+    });
   });
 
   $('a.navLink').bind('click',function() { 
@@ -160,7 +152,6 @@ $(document).ready(function() {
     var visiDiv = "div"+$(this).attr('href');    
     $(visiDiv).fadeIn();
 
-    // 
     $('ul.nav li').css({ 'background-color' : '#fff' });   
 
     // Change tab color to reflect selection
@@ -170,13 +161,4 @@ $(document).ready(function() {
 
   });
 
-  // A starting point 
-  // $('div.task').draggable({});
-/*
-  $('div#wrapper').on("click", "span.handle", function() {
-
-    console.log('next step, boss');
-    $(this).parent().draggable({});
-  }); 
-*/
 });
